898ac38a7f164d776537b63de4ed050a5df07cb0
CougarTasker <cougartasker@outlook.com>
2020-11-15 23:07:52 +0000

    integration merge from the OpStack branch. adding the Opstack class and its tests.

---
+++ calculator/src/main/java/calculator/OpStack.java
@@ -0,0 +1,59 @@
+package calculator;
+
+import java.util.function.BooleanSupplier;
+
+/**
+ * A facade class just allowing the storage of operator symbols to hide the details of the stack
+ * class. this class will be used in the shunting yard algorithm as it needs a stack just for the
+ * symbols.
+ * 
+ * @see Stack
+ * @author Cougar Tasker
+ *
+ */
+public class OpStack {
+  private Stack opStack;
+
+  /**
+   * Constructor for creating a new stack of operator symbols.
+   */
+  public OpStack() {
+    this.opStack = new Stack();
+  }
+
+  /**
+   * Add an operator to be stored at the top of the stack.
+   * 
+   * @param i the operator symbol to be added.
+   */
+  public void push(Symbol i) {
+    opStack.push(new Entry(i));
+  }
+
+  /**
+   * Return the operator that is on the top of the stack. this will remove this operator from the
+   * stack. if the stack is empty this will throw an exception.
+   * 
+   * @return the operator symbol that was on the stack
+   * @throws EmptyStackException Thrown when the stack is empty. cannot remove an element from an
+   *         empty stack.
+   */
+  public Symbol pop() throws EmptyStackException {
+    try {
+      return opStack.pop().getSymbol();
+    } catch (BadTypeException e) {
+      // this block cannot be reached since this class does not support adding other types of data
+      return null;
+    }
+  }
+
+  /**
+   * Check if the stack is empty.
+   * 
+   * @return true if there is no operators on the stack
+   */
+  public boolean isEmpty() {
+    return opStack.size() == 0;
+  }
+
+}
---
+++ calculator/src/test/java/calculator/OpStackTest.java
@@ -0,0 +1,83 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.Duration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OpStackTest {
+  OpStack stackInstance = null;
+  public static final int MAX = 100000;
+  int symbolCount = 0;
+
+  @BeforeEach
+  void before() {
+    stackInstance = new OpStack();
+    symbolCount = Symbol.values().length;
+  }
+
+  @Test
+  void testPush() {
+    stackInstance.push(Symbol.PLUS);
+    stackInstance.push(Symbol.MINUS);
+    stackInstance.push(Symbol.TIMES);
+  }
+
+  @Test
+  void testPop() {
+    assertThrows(EmptyStackException.class, () -> stackInstance.pop(),
+        "cannot remove operator from an empty stack");
+  }
+
+  @Test
+  void testPushPop() throws EmptyStackException {
+    stackInstance.push(Symbol.PLUS);
+    stackInstance.push(Symbol.MINUS);
+    stackInstance.push(Symbol.TIMES);
+
+    assertEquals(Symbol.TIMES, stackInstance.pop(), "pop should return the last element");
+    assertEquals(Symbol.MINUS, stackInstance.pop(),
+        "pop should return elements in the reverse order");
+    assertEquals(Symbol.PLUS, stackInstance.pop(),
+        "pop should return elements in the reverse order");
+    assertThrows(EmptyStackException.class, () -> stackInstance.pop(),
+        "after pushing and poping an empty stack should still throw and exception");
+  }
+
+  @Test
+  void testEmpty() throws EmptyStackException {
+    assertTrue(stackInstance.isEmpty(), "a new stack should be empty");
+    stackInstance.push(Symbol.INVALID);
+    assertFalse(stackInstance.isEmpty(), "a stack containting somthing shouldnt be empty");
+    stackInstance.pop();
+    assertTrue(stackInstance.isEmpty(),
+        "a stack should be empty after the last element is removed");
+  }
+
+  @Test
+  void testMany() throws EmptyStackException {
+    for (int i = 0; i <= MAX; i++) {
+      stackInstance.push(Symbol.values()[i % symbolCount]);
+    }
+    for (int i = MAX; i >= 0; i--) {
+      assertFalse(stackInstance.isEmpty(), "a stack containting somthing shouldnt be empty");
+      assertEquals(i % symbolCount, stackInstance.pop().ordinal(),
+          "the order should be maintaned even with large numbers");
+    }
+    assertTrue(stackInstance.isEmpty(),
+        "a stack should be empty after the last element is removed");
+    assertThrows(EmptyStackException.class, () -> stackInstance.pop(),
+        "cannot remove an entry from an empty stack");
+  }
+
+  @Test
+  void testPerformance() {
+    assertTimeout(Duration.ofNanos(MAX * 1000), () -> {
+      for (int i = 0; i <= MAX; i++) {
+        stackInstance.push(Symbol.values()[i % symbolCount]);
+        stackInstance.pop();
+        stackInstance.isEmpty();
+      }
+    }, "The stack should return quickly");
+  }
+}
